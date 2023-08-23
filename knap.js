8a9aab491c5b3c4827771092d623347bdd42677a
Freja <frejaeb@gmail.com>
2023-08-23 19:47:57 +0200

    alkohol

--- knap.js
+++ knap.js
@@ -1,7 +1,22 @@
+let alkohol = "indeholder alkohol";
+let alkoholFri = "alkoholfri";
+
+
 const minKnap = document.querySelector("#knapElement");
 
 minKnap.addEventListener("click", functionDerKaldesVedKlik);
 
 function functionDerKaldesVedKlik() {
   console.log("du har klikket på knappen");
+}
+
+
+
+
+if(alkohol){
+    console.log("indeholder alkohol");
+   
+}
+else{
+    console.log("alkoholfri")
 }
