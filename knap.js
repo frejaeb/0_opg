6680b22b32005872407fe2088ad00ae237aa9138
Freja <frejaeb@gmail.com>
2023-08-23 19:15:24 +0200

    knap

---
+++ knap.js
@@ -0,0 +1,7 @@
+const minKnap = document.querySelector ("#knapElement");
+
+minKnap. addEventListener ("click",functionDerKaldesVedKlik);
+
+function functionDerKaldesVedKlik () {
+    console.log ("du har klikket på knappen");
+}
